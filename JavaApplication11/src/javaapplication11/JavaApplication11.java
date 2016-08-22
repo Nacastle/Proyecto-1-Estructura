7396eb7e37d10e27fb62fa581aced91a4e083d42
Bryan Mejia <Alexis@Bryans-MacBook-Air.local>
2016-08-21 21:31:24 -0600

    Fin?

--- JavaApplication11/src/javaapplication11/JavaApplication11.java
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
